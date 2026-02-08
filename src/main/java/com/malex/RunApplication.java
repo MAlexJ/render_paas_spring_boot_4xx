93d9265ac18eddd5c3eb22387916cfb3a95f275e
malexj <alexmaximovj@gmail.com>
2026-02-08 20:03:25 +0200

    Add vers info

--- src/main/java/com/malex/RunApplication.java
+++ src/main/java/com/malex/RunApplication.java
@@ -1,13 +1,28 @@
 package com.malex;
 
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.info.BuildProperties;
 
+@Slf4j
 @SpringBootApplication
+@RequiredArgsConstructor
 public class RunApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RunApplication.class, args);
-    }
+  private final BuildProperties buildProperties;
 
+  public static void main(String[] args) {
+    SpringApplication.run(RunApplication.class, args);
+  }
+
+  @PostConstruct
+  public void printVersion() {
+    log.info(
+        "Application version: {} (built at {})",
+        buildProperties.getVersion(),
+        buildProperties.getTime());
+  }
 }
